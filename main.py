0b0a54c9ffc4f52deba31a7b1ef1f3c3eaa1d791
1calango <gls16@aluno.ifnmg.edu.br>
2026-09-24 11:13:55 -0300

    Adiciona projeto Flet

---
+++ main.py
@@ -0,0 +1,15 @@
+import flet as ft
+
+
+def main(page: ft.Page):
+    page.title = "Teste do Flet"
+
+    texto = ft.Text(
+        "Flet funcionando!",
+        size=30
+    )
+
+    page.add(texto)
+
+
+ft.run(main)
